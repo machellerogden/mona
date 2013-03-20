faa84b7b8b7bb61b452ec364134680d58a3a87c2
Mac Heller-Ogden <mac@pixeldreamer.net>
2013-03-20 18:25:23 -0500

    cleaning up my mess

--- index.js
+++ index.js
@@ -1,34 +1,18 @@
 (function(){
 
     var _ = require('underscore'),
-        Deferred = require('JQDeferred'),
         mona;
 
     function Mona(defaults) {
         defaults = defaults || {};
-        function lift (value, key, object) {
-            if (_.isObject(value)) {
-                if (value.value === 'value') {
-                    object[key] = value.value;
-                } else if (_.isBoolean(value.required)) {
-                    delete object[key];
-                }
-            }
-        }
-        function getRequiredKey (value, key) {
-            if ( _.isBoolean(value.required) && (value.required === true) ) {
-                return key;
-            }
-        }
         return function Constructor(config) {
-                var methodKeys, methods, properties, objectProperties, configKeys, requiredKeys;
+                var methodKeys, methods, properties, requiredKeys;
             if (_.isObject(defaults)) {
                 dMethodKeys = _.keys(_.functions(defaults));
                 dMethods = _.pick(defaults, dMethodKeys);
                 dProperties = _.omit(defaults, dMethodKeys);
-                dRequiredKeys = _.reject(_.map(dProperties, getRequiredKey), _.isUndefined);
+                dRequiredKeys = _.keys(_.pick(dProperties, "monarequired"));
                 if (_.has(config, dRequiredKeys)) {
-                    _.each(defaults, lift);
                     _.extend(this, _.defaults(config, defaults));
                     return this;
                 } else {
